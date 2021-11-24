bb8c68662f9cc2c45ae952cc7e9dfcf502d23b35
Anastatia <anastatiad013@gmail.com>
2021-11-24 12:37:15 -0600

    trial 2, really struggled to add this to github

---
+++ setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+
+setup (
+    name='vector',
+    packages = ['vector'],
+    version='0.1',
+    license = 'MIT',
+    description = 'This is a simple vector package to define and compute the magnitude of a vector',
+    author = 'Anastatia Dorval',
+    author_email = 'anastatiad013@gmail.com',
+    url = 'https://github.com/AnastatiaDorval/vector.git',
+    download_url = '',
+    keywords = ['vector', 'magnitude', 'python'],
+    install_requires = [
+        ],
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Physics Undergrads',
+        'Topic :: Math :: Simple Calculations',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        ],
+)
